dd0e7378b18ac4537a31dd016d7180d2f210530e
RenDiego07 <dfflores@espol.edu.ec>
2025-07-07 14:23:36 -0500

    structure for each test

---
+++ features/steps/todo_list_steps.py
@@ -0,0 +1,16 @@
+from behave import *
+from todo_list import ToDoList  # importa tu clase real
+
+@given('a new to-do list')
+def step_impl(context):
+    context.todo = ToDoList()
+
+@when('I add a task with title "{title}" and description "{desc}", priority "{priority}" and due date "{due}"')
+def step_impl(context, title, desc, priority, due):
+    context.todo.add_task(title, desc, priority, due)
+
+@then('the task list should contain {count:d} task')
+@then('the task list should contain {count:d} tasks')
+def step_impl(context, count):
+    assert len(context.todo.tasks) == count
+
